642e8c9e81b5f0aaa4a74c57f3ec2f4221ed09e2
Regis <regisnobel95@gmail.com>
2023-06-07 23:20:15 -0400

    added SC original python file

---
+++ simple-calculator.py
@@ -0,0 +1,24 @@
+print("Welcome to the basic calculator. Please follow the prompts below")
+
+value1 = float(input("What is your first value? "))
+value3 = input("Enter your operator:")
+value2 = float(input("What is your second value? "))
+answer = str()
+
+
+if value3 == "+":
+  answer = value1 + value2
+  
+elif value3 == "-":
+  answer = value1 - value2
+  
+elif value3 == "*":
+  answer = value1 * value2
+  
+elif value3 == "/":
+  answer = value1 / value2
+  
+else:
+  print ("Invalid Operator")
+
+print("The answer is:", answer)
