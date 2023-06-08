5ac269fc04d4607d3321aa68e6944290afac763c
Regis <regisnobel95@gmail.com>
2023-06-08 18:13:47 -0400

    Updated the codebase to accept a string of input

--- simple-calculator.py
+++ simple-calculator.py
@@ -1,26 +1,20 @@
 print("Welcome to the basic calculator. Please follow the prompts below")
 
-
-#testing git
-value1 = float(input("What is your first value? "))
-value3 = input("Enter your operator:")
-value2 = float(input("What is your second value? "))
-answer = str()
-
-
-if value3 == "+":
-  answer = value1 + value2
+while True:
   
-elif value3 == "-":
-  answer = value1 - value2
+  value = input("Enter a calcualtion or quit to exit")
   
-elif value3 == "*":
-  answer = value1 * value2
-  
-elif value3 == "/":
-  answer = value1 / value2
-  
-else:
-  print ("Invalid Operator")
 
-print("The answer is:", answer)
+  if value.lower() == 'quit':
+  
+    break
+  
+  if any(char.isalpha() for char in value):
+    print("Invalid Operation. Letters are not allowed. ")
+  
+  try:
+      answer = eval(value)
+      print("The answer is:", answer)
+      
+  except:
+      print("Invalid Operation. please try again or type quit to exit")
