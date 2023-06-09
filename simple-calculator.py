a14e0b6f04aadfac87c7b8db29fc0ccd1e1cf5b8
Regis <regisnobel95@gmail.com>
2023-06-09 16:54:19 -0400

    reduce security issues from using eval with regex

--- simple-calculator.py
+++ simple-calculator.py
@@ -1,16 +1,23 @@
+import re #import regex module
+
 print("Welcome to the basic calculator. Please follow the prompts below")
+
 # I'm adding my random update to test git pull/push on multiple team members
 while True:
   
-  value = input("Enter a calcualtion or quit to exit")
+  value = input("Enter a calcualtion or quit to exit: ")
   
-
+  # check if user wants to quit
   if value.lower() == 'quit':
   
     break
   
-  if any(char.isalpha() for char in value):
-    print("Invalid Operation. Letters are not allowed. ")
+  # use regex to check if the input is a valid calculation
+  # the regex pattern '^[0-9+\-*/().\s]*$' matches any combination of digits, basic arithmetic operators, parentheses, and whitespace
+  # the '^' and '$' anchors ensure that the entire string must match this pattern
+  if not re.match('^[0-9+\-*/()%.\s]*$', value):
+    print("Invalid calculation. Only numbers and basic arithmetic operators are allowed. Please try again.")
+    continue
   
   try:
       answer = eval(value)
